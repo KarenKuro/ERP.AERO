6324f1275b32f2fb3ae98cb736b10da22bbd34da
KarenKuro <kkarenk888@gmail.com>
2025-05-30 17:10:25 +0300

    Stop tracking dist folder

--- dist/middlewares/error.middleware.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const errorHandler = (err, _req, res, _next) => {
-    console.error("Error:", err);
-    const status = err.status || 500;
-    const message = err.message || "Internal Server Error";
-    res.status(status).json({ error: message });
-};
-exports.default = errorHandler;
